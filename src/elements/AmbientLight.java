2e45c95fd8c843c42ccee0f0bd9431c360497875
avishai elia <avishome@gmail.com>
2019-07-01 10:52:37 +0300

    doc

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -1,4 +1,7 @@
 package elements;
+/**
+ * The power of color that the object return even when the light trun off.
+ */
 
 import primitives.Color;
 
@@ -6,16 +9,36 @@ public class AmbientLight {
     protected Color RGB;
     protected double Ka;
 
+    /**
+     * contractor
+     * @param RGB Color of the AmbientLight
+     * @param ka between 0-1 . power of AmbientLight
+     */
     public AmbientLight(Color RGB, double ka) {
         this.RGB = new Color(RGB) ;
         Ka = ka;
     }
+
+    /**
+     *  help func to calc the Color of AmbientLight.
+     * @return the Color of AmbientLight after the power calc
+     */
     public Color GetIntensity(){
         return new Color(this.RGB.scale(Ka));
     }
-    public AmbientLight(){
 
+    /**
+     * defult constractor
+     */
+    public AmbientLight(){
+        this.RGB = new Color(0,0,0) ;
+        Ka = 0.5;
     }
+
+    /**
+     * copy constractor
+     * @param fillLight the old AmbientLight
+     */
     public AmbientLight(AmbientLight fillLight) {
         RGB = new Color(fillLight.RGB);
         Ka = fillLight.Ka;
--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -42,6 +42,10 @@ public class Camera {
         this.CamLeft = CamFront.crossProduct(CamUp).normalize();
     }
 
+    /**
+     * this a copy constractor
+     * @param cam old object not null.
+     */
     public Camera(Camera cam) {
         this.position = cam.getPosition();
         this.CamUp = cam.getCamUp();
@@ -49,25 +53,35 @@ public class Camera {
         this.CamFront = cam.getCamFront();
     }
 
-
+    /**
+     * the func create ray that out from the camera.
+     * @param pixelsX pixelsX of the ray
+     * @param pixelsY pixelsY of the ray
+     * @param Rx num of pixelsX in picture
+     * @param Rj num of pixelsY in picture
+     * @param distance distance from camera to plane of picture
+     * @param width width of plane
+     * @param height height of plane
+     * @return A ray from camera to scene.
+     */
     public ray constructRayThroughPixel(int pixelsX, int pixelsY,
                                         double Rx, double Rj,
                                         double distance,
                                         double width, double height){
 
         double distanceX=width/pixelsX;
-        double distanceY=height/pixelsY;
+        double distanceY=height/pixelsY; //clac width and height of pixel
         double FromLeftPixelXNotUse = distanceX*Rx;
         double FromTopPixelYNotUse = distanceY*Rj;
         double CenterFromLeftPixelX = FromLeftPixelXNotUse + (distanceX/2);
-        double CenterFromTopPixelY = FromTopPixelYNotUse + (distanceY/2);
+        double CenterFromTopPixelY = FromTopPixelYNotUse + (distanceY/2); //clac the distance of pixel from top plan
 
         Point3D centerPlane = position.add(CamFront.multScalar(distance));
         vector PlaneTop = CamUp.multScalar(height);
-        vector PlaneLeft = CamLeft.multScalar(width);
+        vector PlaneLeft = CamLeft.multScalar(width); // find vectors of plane
 
         Point3D TopOfPlane = centerPlane.add(PlaneTop.multScalar(0.5));
-        Point3D TopLeftOfPlane = TopOfPlane.add(PlaneLeft.multScalar(0.5));
+        Point3D TopLeftOfPlane = TopOfPlane.add(PlaneLeft.multScalar(0.5)); // find the point of pixel on plane
 
         // need substract from point, so change direction of vector
         Point3D YXonPlane = TopLeftOfPlane.add(CamUp.multScalar(CenterFromTopPixelY*(-1))).add(CamLeft.multScalar(CenterFromLeftPixelX*(-1)));
@@ -76,6 +90,20 @@ public class Camera {
 
         return new ray(position,YXonPlane.substract(position).normalize());
     }
+
+    /**
+     * the func create List of ray that out from the camera to scene. the rays out around of point on plane of picture and meeting after FoucsDis. It is get feel of foucs
+     * @param pixelsX pixelsX of the ray
+     * @param pixelsY pixelsY of the ray
+     * @param Rx num of pixelsX in picture
+     * @param Rj num of pixelsY in picture
+     * @param distance distance from camera to plane of picture
+     * @param width width of plane
+     * @param height height of plane
+     * @param FoucsDis Distance from plane to rays meeting
+     * @param Sribua The distance between rays when they out from  plane.
+     * @return List of rays.
+     */
     public List<ray> constructRaysThroughPixel(int pixelsX, int pixelsY,
                                         double Rx, double Rj,
                                         double distance,
@@ -105,6 +133,14 @@ public class Camera {
 
     }
 
+    /**
+     * help func to clac of fucos rays
+     * @param FoucsDis Distance from plane to rays meeting
+     * @param Sribua The distance between rays when they out from  plane.
+     * @param YXonPlane the Point3D on place
+     * @param FromCam the vector from camera
+     * @return 4 rays and the FromCam ray last in 5st.
+     */
     public List<ray> GetFocus(double FoucsDis, double Sribua, Point3D YXonPlane, vector FromCam){
         List<ray> rays =new ArrayList<>();
         if(!Focus.enable) { // for spear time
@@ -117,6 +153,7 @@ public class Camera {
         Point3D PL2 = YXonPlane.add(CamUp.multScalar(Sribua/2)).add(CamLeft.multScalar(-Sribua/2));
         Point3D PL3 = YXonPlane.add(CamUp.multScalar(-Sribua/2)).add(CamLeft.multScalar(-Sribua/2));
         Point3D PL4 = YXonPlane.add(CamUp.multScalar(-Sribua/2)).add(CamLeft.multScalar(Sribua/2));
+        //clac every ray
 
         ray L1 = new ray(PL1,PointF.substract(PL1).normalize());
         ray L2 = new ray(PL2,PointF.substract(PL2).normalize());
@@ -132,34 +169,66 @@ public class Camera {
     }
     /*Getters and Setters*/
 
+    /**
+     *
+     * @return Point3D position
+     */
     public Point3D getPosition() {
         return new Point3D(position);
     }
 
+    /**
+     *
+     * @param position set new point
+     */
     public void setPosition(Point3D position) {
         this.position = new Point3D(position);
     }
 
+    /**
+     *
+     * @return vector getCamFront
+     */
     public vector getCamFront() {
         return new vector(CamFront);
     }
 
+    /**
+     *
+     * @param camFront set vector camFront
+     */
     public void setCamFront(vector camFront) {
         CamFront = new vector(camFront);
     }
 
+    /**
+     *
+     * @return vector CamUp
+     */
     public vector getCamUp() {
         return new vector(CamUp);
     }
 
+    /**
+     *
+     * @param camUp set the vector camUp
+     */
     public void setCamUp(vector camUp) {
         CamUp = new vector(camUp);
     }
 
+    /**
+     *
+     * @return vector CamLeft
+     */
     public vector getCamLeft() {
         return new vector(CamLeft);
     }
 
+    /**
+     *
+     * @param camLeft set the vector camLeft
+     */
     public void setCamLeft(vector camLeft) {
         CamLeft = new vector(camLeft);
     }
